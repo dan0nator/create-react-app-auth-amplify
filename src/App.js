c9f189b8f2d86417b1b70b40cab342527e6f528b
Dan0nator <daniel.e.mcclain@gmail.com>
2021-02-18 10:22:28 -0600

    revert testing

--- src/App.js
+++ src/App.js
@@ -6,31 +6,27 @@ import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
-function App() {
-    const[file, setFile] = useState();
-    const[uploaded, setUploaded] = useState(false);
-
-    return (
-        <div className="App">
-            <input type="file" onChange={(e) => setFile(e.target.files[0])} />
-            <button onClick={async () => {
-                const storageResult = await Storage.put('file.png', file, {
-                    level: 'public',
-                    type: 'image/png'
-                })
-                // Insert predictions code here later
-                setUploaded(true)
-                console.log(storageResult);
-            }}>Upload Document</button>
-
-            <div>
-                {uploaded
-                    ? <div>Your document is uploaded!</div>
-                    : <div>Upload a document to get started</div>}
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Textract document application
+          </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+          </a>
+                </header>
             </div>
-        </div>
-    );
-
+        );
+    }
 }
 
 export default withAuthenticator(App, true);
