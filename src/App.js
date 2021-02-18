ab5904d22a0ed9989e3328fca6dce20ff579be98
Dan0nator <daniel.e.mcclain@gmail.com>
2021-02-18 16:35:58 -0600

    added test button

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
+                    <button>test</button>
                     <p>
                         Textract document application
           </p>
