f5fc222f80a6a373b261b318b99e45a4a46c9d6a
Dan0nator <daniel.e.mcclain@gmail.com>
2021-02-18 16:01:57 -0600

    removed paragraph html

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <p>
+
                         <button onClick={async () => {
                             const storageResult = await Storage.put('testfile.png', file, {
                                 level: 'public',
@@ -24,7 +24,7 @@ class App extends Component {
                             setUploaded(true)
                             console.log(storageResult);
                         }}>Upload</button>
-          </p>
+
                     <a
                         className="App-link"
                         href="https://reactjs.org"
