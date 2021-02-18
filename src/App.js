a8aa78d04ba60ff6d61957d849c797ab690ead83
Dan0nator <daniel.e.mcclain@gmail.com>
2021-02-18 10:49:17 -0600

    test upload

--- src/App.js
+++ src/App.js
@@ -24,21 +24,41 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Textract document application
-          </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-          </a>
-                </header>
+                <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+                <button onClick={async () => {
+                    const storageResult = await Storage.put('file.png', file, {
+                        level: 'public',
+                        type: 'image/png'
+                    })
+                    // Insert predictions code here later
+                    setUploaded(true)
+                    console.log(storageResult);
+                }}>Upload</button>
+
+                <div>
+                    {uploaded
+                        ? <div>Your image is uploaded!</div>
+                        : <div>Upload a photo to get started</div>}
+                </div>
             </div>
+
+
+          //  <div className="App">
+          //      <header className="App-header">
+          //          <img src={logo} className="App-logo" alt="logo" />
+          //          <p>
+          //              Textract document application
+          //</p>
+          //          <a
+          //              className="App-link"
+          //              href="https://reactjs.org"
+          //              target="_blank"
+          //              rel="noopener noreferrer"
+          //          >
+          //              Learn React
+          //</a>
+          //      </header>
+          //  </div>
         );
     }
 }
